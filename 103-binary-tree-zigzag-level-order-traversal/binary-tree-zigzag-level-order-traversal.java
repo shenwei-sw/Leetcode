93c7f3f84ef3292fe50b82dc4bf83c9d8c552663
Wei Shen <weishen3269@gmail.com>
2025-02-10 22:48:40 -0500

    Time: 0 ms (100.00%) | Memory: 42.2 MB (57.69%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -18,23 +18,18 @@ class Solution {
         List<List<Integer>> ret = new LinkedList<>();
         if(root == null) return ret;
 
-        Queue<TreeNode> list = new LinkedList<TreeNode>();
-        list.add(root);
-        boolean isReverse = false;
-
-        while(!list.isEmpty()){
-            int size = list.size();
-            LinkedList<Integer> level = new LinkedList<>();
-            for(int i = 0; i < size; i++){
-                TreeNode node = list.poll();
-                if(isReverse) level.addFirst(node.val); 
-                else level.add(node.val);
-                if(node.left != null) list.add(node.left);
-                if(node.right != null) list.add(node.right);
-            }
-            ret.add(level);
-            isReverse = !isReverse;
-        }
+        dfs(root, 0, ret);
         return ret;
+    }
+
+    public void dfs(TreeNode node, int level, List<List<Integer>> ret){
+        if(level == ret.size())
+            ret.add(new ArrayList<>());
+        
+        if(level % 2 == 0) ret.get(level).add(node.val);
+        else ret.get(level).add(0, node.val);
+
+        if(node.left != null) dfs(node.left, level + 1, ret);
+        if(node.right != null) dfs(node.right, level + 1, ret);
     }
 }
