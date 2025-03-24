874732ebec848451499f6cb837fab60c9b6b1903
Wei Shen <weishen3269@gmail.com>
2025-03-24 18:42:09 -0400

    Time: 15 ms (77.48%) | Memory: 44.1 MB (94.46%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,22 +1,15 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        Map<Integer, Integer> map = new HashMap<>();
-        return dfs(coins, amount, map);        
-    }
-
-    public int dfs(int[] coins, int amount, Map<Integer, Integer> map){
-        if(amount < 0) return -1;
-        if(amount == 0) return 0;
-        if(map.containsKey(amount)) return map.get(amount);
-        int min = Integer.MAX_VALUE;
-        for(int i = 0; i < coins.length; i++){
-            int ret = dfs(coins, amount - coins[i], map);
-            if(ret >= 0 && ret < min){
-                min = ret + 1;
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+        for(int i = 1; i <= amount; i++){
+            for(int j = 0; j < coins.length; j++){
+                if(coins[j] <= i){
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+                }
             }
         }
-        min = min == Integer.MAX_VALUE? -1 : min;
-        map.put(amount, min);
-        return map.get(amount);
+        return dp[amount] == amount + 1? -1 : dp[amount];
     }
 }
