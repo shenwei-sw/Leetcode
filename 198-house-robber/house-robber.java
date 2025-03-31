d71d9fff47f53afaabce916f97e3be5c6c42f306
Wei Shen <weishen3269@gmail.com>
2025-03-31 09:51:02 -0400

    Time: 0 ms (100.00%) | Memory: 41.1 MB (59.93%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,12 +1,14 @@
 class Solution {
     public int rob(int[] nums) {
         int len = nums.length;
-        int[] dp = new int[len + 1];
-        dp[0] = 0;
-        dp[1] = nums[0];
+        int pre = 0;
+        int cur = nums[0];
+        int next;
         for(int i = 2; i <= len; i++){
-            dp[i] = Math.max(dp[i - 2] + nums[i - 1], dp[i - 1]);
+            next = Math.max(pre + nums[i - 1], cur);
+            pre = cur;
+            cur = next;
         }
-        return dp[len];
+        return cur;
     }
 }
