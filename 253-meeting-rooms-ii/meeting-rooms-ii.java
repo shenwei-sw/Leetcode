a032539c850fb6428fe287f3b1b13615d559a086
Wei Shen <weishen3269@gmail.com>
2025-04-03 11:58:53 -0400

    Time: 10 ms (18.06%) | Memory: 46.1 MB (66.09%) - LeetSync

---
+++ 253-meeting-rooms-ii/meeting-rooms-ii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minMeetingRooms(int[][] intervals) {
+        if(intervals == null || intervals.length == 0) return 0;
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        heap.offer(intervals[0][1]);
+        for(int i = 1; i < intervals.length; i++){
+            if(intervals[i][0] >= heap.peek()){
+                heap.poll();
+            }
+            heap.offer(intervals[i][1]);
+        }
+
+        return heap.size();
+    }
+}
