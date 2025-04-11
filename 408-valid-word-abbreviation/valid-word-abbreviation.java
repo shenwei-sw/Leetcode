fd57ecc5932ea9574238f246ee57970973025c45
Wei Shen <weishen3269@gmail.com>
2025-04-10 23:16:10 -0400

    Time: 0 ms (100.00%) | Memory: 42 MB (44.80%) - LeetSync

---
+++ 408-valid-word-abbreviation/valid-word-abbreviation.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean validWordAbbreviation(String word, String abbr) {
+        int lenw = word.length();
+        int lena = abbr.length();
+        int ia = 0;
+        int iw = 0;
+        while(ia < lena && iw < lenw){
+            char ch = abbr.charAt(ia);
+            if(ch == word.charAt(iw)){
+                ia++;
+                iw++;
+            }
+            else if(ch > '0' && ch <= '9'){
+                int len = 0;
+                while(ia < lena && isDigit(ch)){
+                    len = len * 10 + ch - '0';
+                    ia++;
+                    if(ia != lena) ch = abbr.charAt(ia);
+                }
+                iw += len;
+            }
+            else return false;
+        }
+        return ia == lena && iw == lenw;
+    }
+
+    public boolean isDigit(char ch){
+        return ch >= '0' && ch <= '9';
+    }
+}
