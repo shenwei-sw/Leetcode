1ae1c5e0fccf5129c79104ecb693e64aac09f429
Wei Shen <weishen3269@gmail.com>
2025-04-14 14:18:05 -0400

    Time: 7 ms (64.32%) | Memory: 44.7 MB (73.12%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -9,31 +9,15 @@
  */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root == null) return null;
-        Stack<TreeNode> stack = new Stack<>();
-        Map<TreeNode, TreeNode> map = new HashMap<>();
-        stack.push(root);
-        map.put(root, null);
-        while(!stack.isEmpty()){
-            TreeNode node = stack.pop();
-            if(node.left != null){
-                stack.push(node.left);
-                map.put(node.left, node);
-            }
-            if(node.right != null){
-                stack.push(node.right);
-                map.put(node.right, node);
-            }
-        }
-        Set<TreeNode> set = new HashSet<>();
-        while(p != null){
-            set.add(p);
-            p = map.get(p);
-        }
+        return dfs(root, p, q);
+    }
+    public TreeNode dfs(TreeNode node, TreeNode p, TreeNode q){
+        if(node == null || node == p || node == q) return node;
 
-        while(!set.contains(q)){
-            q = map.get(q);
-        }
-        return q;
+        TreeNode left = dfs(node.left, p, q);
+        TreeNode right = dfs(node.right, p, q);
+        if(left != null && right != null) return node;
+        else if (left != null) return left;
+        else return right;
     }
 }
