6607c295160292720587c9b129b45091a1cd39f5
Wei Shen <weishen3269@gmail.com>
2025-04-13 23:32:01 -0400

    Time: 0 ms (100.00%) | Memory: 46.2 MB (37.09%) - LeetSync

---
+++ 1816-lowest-common-ancestor-of-a-binary-tree-iv/lowest-common-ancestor-of-a-binary-tree-iv.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode[] nodes) {
+        if(nodes == null ||nodes.length == 0) return null;
+        if(nodes.length == 1) return nodes[0];
+
+        return dfs(root, nodes);       
+    }
+
+    public TreeNode dfs(TreeNode node, TreeNode[] nodes){
+        if(node == null) return node;
+        for(TreeNode n : nodes){
+            if (n == node) return node;
+        }
+        TreeNode left = dfs(node.left, nodes);
+        TreeNode right = dfs(node.right, nodes);
+        if(left != null && right != null) return node;
+        else if(left != null) return left;
+        else return right;
+    }
+}
