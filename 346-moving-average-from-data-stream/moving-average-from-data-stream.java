8214dac535a083bc318135fe20863a6110ba82e7
Wei Shen <weishen3269@gmail.com>
2025-04-14 18:15:23 -0400

    Time: 40 ms (87.77%) | Memory: 49.7 MB (41.94%) - LeetSync

---
+++ 346-moving-average-from-data-stream/moving-average-from-data-stream.java
@@ -0,0 +1,26 @@
+class MovingAverage {
+    int sum;
+    int size;
+    Queue<Integer> queue;
+    public MovingAverage(int size) {
+        sum = 0;
+        this.size = size;
+        queue = new LinkedList<>();
+    }
+    
+    public double next(int val) {
+        sum += val;
+        queue.add(val);
+        if(queue.size() > size){
+            int minus = queue.poll();
+            sum -= minus;
+        }
+        return (double) sum / queue.size();
+    }
+}
+
+/**
+ * Your MovingAverage object will be instantiated and called as such:
+ * MovingAverage obj = new MovingAverage(size);
+ * double param_1 = obj.next(val);
+ */
