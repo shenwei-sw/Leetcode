ecb530680ff5f965dc73d567283f116771b3be4d
Wei Shen <weishen3269@gmail.com>
2025-04-14 15:18:53 -0400

    Time: 23 ms (98.09%) | Memory: 50.1 MB (45.73%) - LeetSync

---
+++ 912-random-pick-with-weight/random-pick-with-weight.java
@@ -0,0 +1,34 @@
+class Solution {
+    int[] sums; 
+    int sum = 0;
+    public Solution(int[] w) {
+        sums = new int[w.length];
+        for(int i = 0; i < sums.length; i++){
+            sum += w[i];
+            sums[i] = sum;
+        }
+    }
+    
+    public int pickIndex() {
+        double target = sum * Math.random();
+        int low = 0;
+        int high = sums.length - 1;
+        while(low < high){
+            int mid = (low + high) / 2;
+            if(target > sums[mid]){
+                low = mid + 1;
+            }
+            else {
+                high = mid;
+            }
+        }
+
+        return low;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(w);
+ * int param_1 = obj.pickIndex();
+ */
