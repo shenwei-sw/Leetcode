9471743a8cd5f79a0610987d25936e72f8b2b7f9
Wei Shen <weishen3269@gmail.com>
2025-04-16 15:20:54 -0400

    Time: 1 ms (85.51%) | Memory: 42 MB (74.00%) - LeetSync

---
+++ 998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isCompleteTree(TreeNode root) {
+        if(root == null) return true;
+
+        boolean isMissing = false;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            TreeNode node = queue.poll();
+            if(node == null){
+                isMissing = true;
+            }
+            else {
+                if(isMissing) return false;
+                queue.offer(node.left);
+                queue.offer(node.right);
+            }
+        }
+        return true;
+    }
+}
