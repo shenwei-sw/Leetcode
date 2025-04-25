37bd9914bc6f1fbb087b75fc2c22e5c51ca4bb30
Wei Shen <weishen3269@gmail.com>
2025-04-24 23:00:49 -0400

    Time: 2 ms (68.97%) | Memory: 44 MB (96.62%) - LeetSync

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.java
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.java
@@ -2,23 +2,13 @@ class Solution {
     public int minimumOperations(int[] nums) {
         if(nums.length == 0) return 0;
 
-        Map<Integer, Integer> map = new HashMap<>();
-        for(int i = 0; i < nums.length; i++){
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        int len = nums.length; 
+        Set<Integer> set = new HashSet<>();
+        for(int i = len - 1; i >= 0; i--){
+            if(set.contains(nums[i]))
+                return i / 3 + 1;
+            set.add(nums[i]);
         }
-
-        if(map.size() == nums.length) return 0;
-        int ret = 0;
-        int i = 0;
-        for(; i < nums.length; i++){
-            map.put(nums[i], map.get(nums[i]) - 1);
-            if(map.get(nums[i]) == 0) map.remove(nums[i]);
-            if(i % 3 == 2){
-                ret++;
-                if(map.size() == nums.length - i - 1) return ret;
-            }
-        }
-        if(i % 3 != 0) ret++;
-        return ret;
+        return 0;
     }
 }
