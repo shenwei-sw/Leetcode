45af25f1de664798c7a4f3aab678aab2ba17874d
Wei Shen <weishen3269@gmail.com>
2025-04-29 00:01:29 -0400

    Time: 8 ms (68.78%) | Memory: 44.9 MB (31.06%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,16 +1,21 @@
 class Solution {
-    int ret = 0;
     public int findTargetSumWays(int[] nums, int target) {
-        dfs(0, 0, nums, target);
-        return ret;
+        int total = Arrays.stream(nums).sum();
+        if(Math.abs(target) > total) return 0;
+        int[][] dp = new int[nums.length][2 * total + 1];
+
+        dp[0][nums[0] + total] += 1;
+        dp[0][-nums[0] + total] += 1;
+
+        for(int i = 1; i < nums.length; i++){
+            for(int sum = -total; sum <= total; sum++){
+                if(dp[i - 1][sum + total] > 0){
+                    dp[i][sum + nums[i] + total] += dp[i - 1][sum + total];        
+                    dp[i][sum - nums[i] + total] += dp[i - 1][sum + total];
+                }
+            }
+        }
+        return dp[nums.length - 1][target + total];
     }
 
-    public void dfs(int index, int sum, int[] nums, int target){
-        if(index == nums.length){
-            if(sum == target) ret++;
-            return;
-        }
-        dfs(index + 1, sum + nums[index], nums, target);
-        dfs(index + 1, sum - nums[index], nums, target);
-    }
 }
