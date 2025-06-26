b2510ad1b8f79613fa8c65809a96b47f3a460f47
Wei Shen <weishen3269@gmail.com>
2025-06-25 22:08:21 -0400

    Time: 6 ms (77.53%) | Memory: 46.6 MB (9.27%) - LeetSync

--- 252-meeting-rooms/meeting-rooms.java
+++ 252-meeting-rooms/meeting-rooms.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean canAttendMeetings(int[][] intervals) {
         if(intervals == null || intervals.length < 2) return true;
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
         for(int i = 1; i < intervals.length; i++){
             if(intervals[i][0] < intervals[i - 1][1]) return false;
         }
